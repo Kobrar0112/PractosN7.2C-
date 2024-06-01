6288578b2b27948ede69cbd09b78c88484f874d4
Kobrar0112 <85014618+Kobrar0112@users.noreply.github.com>
2024-06-01 17:57:24 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+// Определение типа функции из DLL
+typedef bool (*FindCharsFunc)(const char*);
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // Имя библиотеки и имя функции
+    const char* dllName = "FindChars.dll";
+    const char* funcName = "FindCharsInString";
+
+    // Ввод символов от пользователя
+    string inputChars;
+    cout << "Введите символы для поиска: ";
+    getline(cin, inputChars);
+
+    // Загрузка библиотеки
+    HMODULE hLib = LoadLibraryA(dllName);
+    if (!hLib) {
+        cerr << "Не удалось загрузить библиотеку: " << dllName << endl;
+        return 1;
+    }
+
+    // Получение адреса функции
+    FindCharsFunc findChars = (FindCharsFunc)GetProcAddress(hLib, funcName);
+    if (!findChars) {
+        cerr << "Не удалось найти функцию: " << funcName << endl;
+        FreeLibrary(hLib);
+        return 1;
+    }
+
+    // Вызов функции
+    bool result = findChars(inputChars.c_str());
+
+    // Вывод результата
+    if (result) {
+        cout << "Все символы найдены в строке." << endl;
+    }
+    else {
+        cout << "Не все символы найдены в строке." << endl;
+    }
+
+    // Освобождение библиотеки
+    FreeLibrary(hLib);
+
+    return 0;
+}
---
+++ FindChars/dllmain.cpp
@@ -0,0 +1,30 @@
+﻿#include <windows.h>
+#include <cstring>
+#include "pch.h"
+
+//  хранении слова
+static const char* storedWord = "Хэллоу"; 
+
+// Определение функции, которая будет экспортирована из DLL
+extern "C" __declspec(dllexport) bool FindCharsInString(const char* chars) {
+    const char* str = storedWord;
+    while (*chars) {
+        if (!strchr(str, *chars)) {
+            return false;
+        }
+        chars++;
+    }
+    return true;
+}
+
+// Функция DllMain, которая вызывается при подключении/отключении DLL
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
